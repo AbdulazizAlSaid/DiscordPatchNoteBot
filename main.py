05c6360f599db8faee3fb6b6471aa6fff251cbf0
AbdulazizAlSaid <90004014+AbdulazizAlSaid@users.noreply.github.com>
2022-02-15 21:01:15 +0000

    Added a cleaner method of checking for command by lowercasing message, as well as adding more games to the lost of patch notes

--- main.py
+++ main.py
@@ -1,30 +1,47 @@
 import discord
 import os
 import patchScrape
-htmlS = patchScrape.htmlScraper() 
+from keep_alive import keep_alive
+htmlS = patchScrape.dexertoPatchScraper() 
 
 client = discord.Client()
 my_secret = os.environ['TOKEN']
 
+
+  
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
+
+  message.content = message.content.lower()
+  
   if message.author == client.user:
     return
 
-  if message.content.startswith('$hello'):
-    await message.channel.send("Hello!")
 
-  if message.content.startswith('lpn.' or 'LPN.' or 'Lpn.'):
-    activeURL = htmlS.search()
+  if message.content=='$hello':
+    await message.channel.send("Hello! I am the Patch Note Bot! Type $commands to learn what I can do for you!")
+    
+  if message.content == '$commands':
+    await message.channel.send('Commands: \npn. : patch notes, follow those with different games for different patch notes. \nGames: \nlol : League of Legends \ntft : Teamfight Tactics \nvalo : Valorant \napex : Apex Legends')
+  
+  if message.content.startswith('pn.'):
+    if message.content.endswith('lol'):
+      patchCommand = 'lol'
+    elif message.content.endswith('tft'):      
+      patchCommand = 'tft'
+    elif message.content.endswith('valo'):
+      patchCommand = 'valo'
+    elif message.content.endswith('apex'):
+      patchCommand = 'apex'
+    else:
+      await message.channel.send('Incorrect command, please type $commands to learn what I can do for you!')
+      
+    activeURL = htmlS.search(patchCommand)
     print(activeURL)
     await message.channel.send(activeURL)
-
-
-
-
+keep_alive()
 client.run(my_secret)
-
--- patchScrape.py
+++ patchScrape.py
@@ -2,15 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-class htmlScraper:
+class dexertoPatchScraper:
+  patchPath = ''
+
   def __init__(self):
     self.headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.3'}
-    self.url = 'https://www.dexerto.com/league-of-legends/'
+    self.url = 'https://www.dexerto.com'
 
-  def search(self):
+  def search(self, patchCommand):
+    if patchCommand == 'lol':
+      self.url = 'https://www.dexerto.com/league-of-legends/'
+      patchPath = '^https://www.dexerto.com/league-of-legends/lol-patch'
+    if patchCommand == 'tft':
+      self.url = 'https://www.dexerto.com/league-of-legends/'
+      patchPath = '^https://www.dexerto.com/league-of-legends/teamfight-tactics-tft'
+    if patchCommand == 'valo':
+      self.url = 'https://www.dexerto.com/valorant/'
+      patchPath = '^https://www.dexerto.com/valorant/valorant-patch'
+    if patchCommand == 'apex':
+      self.url = 'https://www.dexerto.com/apex-legends/'
+      patchPath = '^https://www.dexerto.com/apex-legends/apex-legends-season-12-patch-notes'
     base = requests.get(self.url)
     soup = BeautifulSoup(base.content, 'html.parser')
-    link = soup.find('a', href=re.compile('^https://www.dexerto.com/league-of-legends/lol-patch-'))
+    link = soup.find('a', href=re.compile(patchPath))
     final_link = link.get('href')
 
     return final_link 
